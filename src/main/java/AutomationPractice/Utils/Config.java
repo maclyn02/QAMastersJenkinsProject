31ab895a73d68d1ddf67dfdbebc11e707c20077b
maclyn02 <mac.afonso@yahoo.com>
2019-07-24 10:20:07 +0100

    added gitignore

--- src/main/java/AutomationPractice/Utils/Config.java
+++
@@ -1,5 +0,0 @@
-package AutomationPractice.Utils;
-
-public class Config {
-    static String gridURL="https://mac.afonso:ocTlTPyJymYtNvf36c8KDOMv7CMpUAgbp6Kl986eIhstQ3zx0N@hub.lambdatest.com/wd/hub";
-}
